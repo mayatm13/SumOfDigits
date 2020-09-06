1a9598ccf1184628556e64d5f5a078d4d71bbc78
Ali Mousavifar <Ali.Mousavifar@gmail.com>
2020-09-06 15:57:37 -0700

    git_review - some of digits- experiment with remote repository

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input  a number(integer): ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while (n != 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            Console.WriteLine("Sum of the digits you entered is: " + sum);
+        }
+    }
+}
