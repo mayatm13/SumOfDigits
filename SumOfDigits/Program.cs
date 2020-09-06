e28fdc02452754edf47a70a24f03e1d590adbbc1
Ali <ali.mousavifar@gmail.com>
2020-09-06 16:26:09 -0700

    git_review - Some Cosmetic changes

--- SumOfDigits/Program.cs
+++ SumOfDigits/Program.cs
@@ -14,7 +14,7 @@ namespace SumOfDigits
                 sum += n % 10;
                 n /= 10;
             }
-            Console.WriteLine("Sum of the digits you entered is: " + sum);
+            Console.WriteLine("The sum of the digits you entered is: " + sum);
         }
     }
 }
